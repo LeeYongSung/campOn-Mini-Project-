ca5b8806fcc7d2de0396136f211f3202b8b061cd
Koomaxx86 <bluejjw86@daum.net>
2023-12-01 16:48:06 +0900

    23.12.01

---
+++ CampOn-React/campon-app/src/components/CampBoardInsertForm.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useRef } from 'react';
+
+function CampBoardInsertForm({ onInsert }) => {
+
+    // state 설정
+    cosnt []
+
+
+    return (
+    )
+}
+
+export default CampBoardInsertForm;
