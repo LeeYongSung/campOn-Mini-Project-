0ab70773cfabd5200680f01d03e75b690cd0d6b6
LeeYongSung <dldydtjd9@naver.com>
2023-12-04 16:28:19 +0900

    11

--- CampOn-React/campon-app/src/components/header/BackCartHeader.jsx
+++ CampOn-React/campon-app/src/components/header/BackCartHeader.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import Linkscript from '../fragment/link';
 const BackCartHeader = () => {
   return (
     <>
-      <Linkscript />
       <header>
           <nav>
               <ul class="main_header">
--- CampOn-React/campon-app/src/containers/admin/AdminProductList.jsx
+++ CampOn-React/campon-app/src/containers/admin/AdminProductList.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import '../App.css'
 
 const AdminProductList = () => {
     const [productList, setProductList] = useState([]);
