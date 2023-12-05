a41502eee68acdf204ef3a47c5babb04f039e0ad
LeeYongSung <dldydtjd9@naver.com>
2023-12-05 23:33:50 +0900

    보드 패키지 누락 추가

--- campon/src/main/java/com/camp/campon/apis/BoardApiController.java
+++ campon/src/main/java/com/camp/campon/apis/BoardApiController.java
@@ -1,3 +1,5 @@
+package com.camp.campon.apis;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
