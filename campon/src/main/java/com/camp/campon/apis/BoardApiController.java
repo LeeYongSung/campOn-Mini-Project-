8b1202b7eda00c038dd1a5a050a9084c50339793
LeeYongSung <dldydtjd9@naver.com>
2023-12-06 09:39:13 +0900

    오류 수정

--- campon/src/main/java/com/camp/campon/apis/BoardApiController.java
+++ campon/src/main/java/com/camp/campon/apis/BoardApiController.java
@@ -1,5 +1,9 @@
 package com.camp.campon.apis;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import com.camp.campon.dto.Board;
 import com.camp.campon.service.BoardService;
