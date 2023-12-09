cbc3b78db3802d24c49b21b50c8c8dd77b9e87b5
Koomaxx86 <bluejjw86@daum.net>
2023-12-09 19:10:29 +0900

    1209 1910

--- CampOn-React/campon-app/src/components/board/CampBoardUpdate.jsx
+++ CampOn-React/campon-app/src/components/board/CampBoardUpdate.jsx
@@ -1,57 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const CampBoardUpdate = ({ review, onSubmit, onDelete, formValues, setFormValues }) => {
-  const handleChange = (event) => {
-    setFormValues({
-      ...formValues,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleFileChange = (event) => {
-    setFormValues({
-      ...formValues,
-      reviewImgfile: event.target.files[0],
-    });
-  };
+const CampBoardUpdate = ({ reservation, reviewTitle, reviewCon, onDelete, handleSubmit, handle }) => {
 
   return (
     <div>
       <h1>캠핑장 리뷰 수정</h1>
       <h4>캠핑장 리뷰을 수정하기 위한 페이지입니다.</h4>
-      <form onSubmit={onSubmit} id="form" enctype="multipart/form-data">
-        <hr />
-        <div>
-          <p>{formValues.campName}</p>
-          <p>{formValues.cpdtName}</p>
+      <hr />
+      <div>
+          <p>{reservation.campName}</p>
+          <p>{reservation.cpdtName}</p>
         </div>
-        <hr />
-        <table>
+      <table>
+        <tbody>
           <tr>
             <td>제목</td>
             <td>
-              <input type="text" name="reviewTitle" value={formValues.reviewTitle} onChange={handleChange} />
+              <input type="text" name="reviewTitle" value={reviewTitle} onChange={handle} />
             </td>
           </tr>
           <tr>
             <td>파일</td>
             <td>
-              <input type="file" name="reviewImgfile" onChange={handleFileChange} />
+              <input type="file" name="reviewImgfile" onChange={handle} />
             </td>
           </tr>
           <tr>
             <td>내용</td>
             <td>
-              <textarea name="reviewCon" cols="40" rows="5" placeholder="내용" value={formValues.reviewCon} onChange={handleChange} />
+              <textarea name="reviewCon" cols="40" rows="5" placeholder="내용" value={reviewCon} onChange={handle} />
             </td>
           </tr>
-        </table>
-        <div>
-          <button type="button" onClick={() => window.history.back(-1)}>목록</button>
-          <button type="submit">수정</button>
-          <button type="button" onClick={onDelete}>삭제</button>
-        </div>
-      </form>
+        </tbody>
+      </table>
+      <div>
+        <Link to="/api/board/index" className='btn'>목록</Link>
+        <button type="button" onClick={handleSubmit}>수정</button>
+        <button type="button" onClick={onDelete}>삭제</button>
+      </div>
     </div>
   );
 };
--- CampOn-React/campon-app/src/components/board/ProductBoardUpdate.jsx
+++ CampOn-React/campon-app/src/components/board/ProductBoardUpdate.jsx
@@ -1,57 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ProductBoardUpdate = ({ pr, onSubmit, onDelete, formValues, setFormValues }) => {
-  const handleChange = (event) => {
-    setFormValues({
-      ...formValues,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleFileChange = (event) => {
-    setFormValues({
-      ...formValues,
-      prImgfile: event.target.files[0],
-    });
-  };
-
+const ProductBoardUpdate = ({ reservation, prTitle, prCon, onDelete, handle, handleSubmit }) => {
   return (
     <div>
       <h1>대여상품 리뷰 수정</h1>
       <h4>대여상품 리뷰을 수정하기 위한 페이지입니다.</h4>
-      <form onSubmit={onSubmit} id="form" enctype="multipart/form-data">
         <hr />
         <div>
-          <p>{formValues.productName}</p>
-          <p>{formValues.productCategory}</p>
+          <p>{reservation.productName}</p>
+          <p>{reservation.productCategory}</p>
         </div>
         <hr />
         <table>
           <tr>
             <td>제목</td>
             <td>
-              <input type="text" name="prTitle" value={formValues.prTitle} onChange={handleChange} />
+              <input type="text" name="prTitle" value={prTitle} onChange={handle} />
             </td>
           </tr>
           <tr>
             <td>파일</td>
             <td>
-              <input type="file" name="prImgfile" onChange={handleFileChange} />
+              <input type="file" name="prImgfile" onChange={handle} />
             </td>
           </tr>
           <tr>
             <td>내용</td>
             <td>
-              <textarea name="prCon" cols="40" rows="5" placeholder="내용" value={formValues.prCon} onChange={handleChange} />
+              <textarea name="prCon" cols="40" rows="5" placeholder="내용" value={prCon} onChange={handle} />
             </td>
           </tr>
         </table>
         <div>
-          <button type="button" onClick={() => window.history.back(-1)}>목록</button>
-          <button type="submit">수정</button>
+        <Link to="/api/board/index" className='btn' >목록</Link>
+          <button type="submit" onClick={handleSubmit}>수정</button>
           <button type="button" onClick={onDelete}>삭제</button>
         </div>
-      </form>
     </div>
   );
 };
